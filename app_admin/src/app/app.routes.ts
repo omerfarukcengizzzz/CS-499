4f0933c112a285ee9edb8c459df9c20f67558ddc
omerfarukcengizzzz <omerfarukcengizzzz@gmail.com>
2026-02-12 23:27:03 -0800

    enhance: add Angular route guard to protect admin SPA routes

--- app_admin/src/app/app.routes.ts
+++ app_admin/src/app/app.routes.ts
@@ -5,12 +5,13 @@ import { TripListingComponent } from './trip-listing/trip-listing.component';
 import { LoginComponent } from './login/login.component';
 import { UserListingComponent } from './user-listing/user-listing.component';
 import { BookingListingComponent } from './booking-listing/booking-listing.component';
+import { authGuard } from './utils/auth.guard';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
-    { path: 'add-trip', component: AddTripComponent },
-    { path: 'edit-trip', component: EditTripComponent },
-    { path: 'users', component: UserListingComponent },
-    { path: 'bookings', component: BookingListingComponent },
-    { path: '', component: TripListingComponent, pathMatch: 'full' }
+    { path: 'add-trip', component: AddTripComponent, canActivate: [authGuard] },
+    { path: 'edit-trip', component: EditTripComponent, canActivate: [authGuard] },
+    { path: 'users', component: UserListingComponent, canActivate: [authGuard] },
+    { path: 'bookings', component: BookingListingComponent, canActivate: [authGuard] },
+    { path: '', component: TripListingComponent, pathMatch: 'full', canActivate: [authGuard] }
 ];
